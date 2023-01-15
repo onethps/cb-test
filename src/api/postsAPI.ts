12b0328206536119b4e6e7eb462b7f60f5f9bb75
evgenij <onethps@gmail.com>
2023-01-15 16:47:26 +0200

    🐞 fix(changed api):

--- src/api/postsAPI.ts
+++ src/api/postsAPI.ts
@@ -1,19 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const postsAPI = {
-  getPosts(search:string) {
+  getPosts(search: string) {
     const searchParams = search.split(" ").join("|");
-    return axios.get(`https://api.thenewsapi.com/v1/news/all?search=${searchParams}`, {
-      params: {
-        language: 'en',
-        search_fields: 'title,description',
-        api_token: process.env.REACT_APP_NEWS_KEY,
-
+    return axios.get(
+      `https://api.thenewsapi.com/v1/news/all?search=${searchParams}`,
+      {
+        params: {
+          language: "en",
+          search_fields: "title,description",
+          api_token: process.env.REACT_APP_NEWS_KEY,
+        },
       },
-    });
+    );
   },
-  getPostById(uuid:string) {
-    return axios.get(`https://api.thenewsapi.com/v1/news/uuid/${uuid}?api_token=${process.env.REACT_APP_NEWS_KEY}`, {
-    });
+
+  getPostById(uuid: string) {
+    return axios.get(
+      `https://api.thenewsapi.com/v1/news/uuid/${uuid}?api_token=${process.env.REACT_APP_NEWS_KEY}`,
+      {},
+    );
   },
 };
